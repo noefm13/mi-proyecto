f05225ba7a96a1780d9c120410fdfa72d2f5e4b0
noefm <noefm0318@gmail.com>
2025-08-05 20:18:43 -0600

    uploadpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://noefm13.github.io',
+  base: 'mi-proyecto',
   vite: {
     plugins: [tailwindcss()]
   }
